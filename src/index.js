0263fe11f454686c4b34a07ab8f8c041571003cd
Michael <mmbmf.mm@gmail.com>
2020-01-30 08:51:37 -0600

    fixing more typos

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App/App';
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter basname='/'>
+    <BrowserRouter>
         <App />
     </BrowserRouter>,
     document.getElementById('root'));
