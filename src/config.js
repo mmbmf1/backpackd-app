acb1dc948cef78003970bcd07c8cdd430486f3af
Michael <mmbmf.mm@gmail.com>
2020-02-13 17:10:31 -0600

    live api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    // API_ENDPOINT: 'https://quiet-scrubland-29000.herokuapp.com/api',
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://quiet-scrubland-29000.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'backpackd-app-auth-token',
     USER:'backpackd-user'
   }
